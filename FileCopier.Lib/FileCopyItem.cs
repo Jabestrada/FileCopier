3c4f7e1caf4bb95de04b9f96b3adcde4cfd891fe
JABE <jabestrada@yahoo.com>
2020-03-15 16:21:53 +0800

    Namespace cleanup

--- FileCopier.Lib/FileCopyItem.cs
+++ FileCopier.Lib/FileCopyItem.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace FileCopier.Lib
-{
+namespace FileCopier.Lib {
     public class FileCopyItem
     {
         public string SourceFile { get; set; }
